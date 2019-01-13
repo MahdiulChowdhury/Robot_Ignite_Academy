10bc206d6d6f5560454d5777c40fa14b8b3ba80a
mahdiulchowdhury <mahdiulc@bu.edu>
2019-01-12 20:53:01 -0500

    new

---
+++ excercise_two/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/optimusprim/Robot_Ignite_Academy/excercise_two/catkin_ws/src
+# Build directory: /home/optimusprim/Robot_Ignite_Academy/excercise_two/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
---
+++ excercise_two/catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/optimusprim/Robot_Ignite_Academy/excercise_two/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/optimusprim/Robot_Ignite_Academy/excercise_two/catkin_ws/devel;/home/optimusprim/Robot_Ignite_Academy/excercise_one/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/optimusprim/Robot_Ignite_Academy/excercise_two/catkin_ws/devel;/home/optimusprim/Robot_Ignite_Academy/excercise_one/catkin_ws/devel;/opt/ros/kinetic" != "" else []
